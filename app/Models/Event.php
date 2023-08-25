5f53af137a37b5b64560453c8a9107913e2b8165
unknown <jungw0npepsi@gmail.com>
2023-08-25 09:39:56 +0700

    models Event

---
+++ app/Models/Event.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function comment_posts()
+    {
+        return $this->hasMany(CommentPost::class);
+    }
+
+    public function event_users()
+    {
+        return $this->hasMany(Eventuser ::class);
+    }
+
+}
