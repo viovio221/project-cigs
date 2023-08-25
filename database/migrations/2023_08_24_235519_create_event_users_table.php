74846c4d895355fb43441897a2c94950c820a08f
unknown <jungw0npepsi@gmail.com>
2023-08-25 07:11:18 +0700

    database

---
+++ database/migrations/2023_08_24_235519_create_event_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('event_id')->nullable();
+            $table->enum('status', ['register', 'presence'])->default('register');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('event_users', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
