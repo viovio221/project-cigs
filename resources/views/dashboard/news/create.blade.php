4ad213588ab233dfcf472a428c0a5d5c6e705878
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-08-30 15:21:08 +0700

    update create news

--- resources/views/dashboard/news/create.blade.php
+++ resources/views/dashboard/news/create.blade.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Message</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>News</title>
   </head>
   <body>
 <div class="container mt-5">
@@ -14,14 +15,14 @@
           <label class="form-label">Title</label>
           <input name="title" type="text" class="form-control @error('title') is-invalid @enderror">
           @error('title')
-            <div class="invalid-feedback">{{ $message }}</div>
+            <div class="invalid-feedback">{{ $news }}</div>
         @enderror
         </div>
         <div class="form-group">
             <label class="form-label">Description</label>
             <input name="description" type="text" class="form-control @error('description') is-invalid @enderror">
             @error('description')
-              <div class="invalid-feedback">{{ $message }}</div>
+              <div class="invalid-feedback">{{ $news }}</div>
           @enderror
           </div>
           <div class="mb-3">
@@ -29,12 +30,14 @@
             <input type="file" name="image" class="form-control @error('image') is-invalid @enderror">
             @error('image')
                 <div class="alert alert-danger mt-2">
-                    {{ $message }}
+                    {{ $news }}
                 </div>
             @enderror
         </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
