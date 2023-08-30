ab359c0bdd0e0eb69e76ef7b907ce632fe160fca
unknown <jungw0npepsi@gmail.com>
2023-08-30 16:17:02 +0700

    remove name

--- database/migrations/2023_08_24_045604_create_messages_table.php
+++ database/migrations/2023_08_24_045604_create_messages_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('message');
