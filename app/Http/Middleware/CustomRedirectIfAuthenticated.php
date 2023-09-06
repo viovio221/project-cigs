0322d06f682b28cbfa52db8e7e7bcdb06cc0674a
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-09-06 07:23:57 +0700

    menambahkan midleware

---
+++ app/Http/Middleware/CustomRedirectIfAuthenticated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomRedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            return redirect('/dashboard');
+        }
+
+        Alert::error('You dont have access to the dashboard page', 'Please log in first')->persistent('Close');
+        return redirect('/login');
+    }
+}
