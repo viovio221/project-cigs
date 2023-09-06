d64cd3d29a84709cfcfb49fee9537f9ce769556f
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-09-06 07:24:33 +0700

    update kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,4 +66,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+    protected $routeMiddleware = [
+        'customRedirect' => \App\Http\Middleware\CustomRedirectIfAuthenticated::class,
+    ];
+
 }
