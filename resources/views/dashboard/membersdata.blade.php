136bca5df24ed7254bbd9e2b9a0860bc3fd90d15
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-09-08 09:48:04 +0700

    add members data

---
+++ resources/views/dashboard/membersdata.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+
+    <title>Members Data</title>
+</head>
+
+<body>
+
+    <!-- SIDEBAR -->
+    <section id="sidebar">
+        <a href="#" class="brand">
+            <i class="fa-solid fa-motorcycle"></i>
+            <span class="text">Wayang Riders</span>
+        </a>
+        <ul class="side-menu top">
+            <li>
+                <a href="/dashboard">
+                    <i class='bx bxs-dashboard'></i>
+                    <span class="text">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="/dashboard/event">
+                    <i class='bx bxs-shopping-bag-alt'></i>
+                    <span class="text">Events</span>
+                </a>
+            </li>
+            <li>
+                <a href="/dashboard/news">
+                    <i class='bx bxs-message-dots'></i>
+                    <span class="text">News</span>
+                    {{-- admin --}}
+                </a>
+            </li>
+            <li class="active">
+                <a href="#">
+                    <i class='bx bxs-group'></i>
+                    <span class="text">Members Data</span>
+                </a>
+            </li>
+            <li>
+                <a href="/dashboard/data_crud">
+                    <i class='bx bx-data'></i> <span class="text">CRUD Riders</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="side-menu">
+            <li>
+                <a href="#" class="logout">
+                    <i class='bx bxs-log-out-circle'></i>
+                    <span class="text">Logout</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- SIDEBAR -->
+
+    <!-- CONTENT -->
+    <section id="content">
+        <!-- NAVBAR -->
+        <nav>
+            <i class='bx bx-menu'></i>
+            <form action="#">
+                <div class="form-input">
+                    <input type="search" placeholder="Cari...">
+                    <button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
+                </div>
+            </form>
+            <input type="checkbox" id="switch-mode" hidden>
+            <label for="switch-mode" class="switch-mode"></label>
+            <a href="#" class="notification">
+                <i class='bx bxs-bell'></i>
+                <span class="num">8</span>
+            </a>
+            <a href="#" class="profile">
+                <img src="{{ asset('images/devani.jpg') }}">
+            </a>
+        </nav>
+        <!-- NAVBAR -->
+
+        <!-- MAIN -->
+        <main>
+            <div class="head-title">
+                <div class="left">
+                    <h1>Members Data</h1>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="/dashboard">Dashboard</a>
+                        </li>
+                        <li><i class='bx bx-chevron-right'></i></li>
+                        <li>
+                            <a class="active" href="/">Landing Page</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </main>
+        <!-- MAIN -->
+    </section>
+    <!-- CONTENT -->
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+</body>
+
+</html>
