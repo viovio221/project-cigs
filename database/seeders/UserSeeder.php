2b5c365dcd1d44e379cdc86b9d5c9ffb46b72390
unknown <jungw0npepsi@gmail.com>
2023-09-13 17:51:37 +0700

    seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(300)->create([
+            'phone_number' => '08123456789', // Ganti dengan nomor telepon yang sesuai
+            'role' => 'member', // Menambahkan kolom "role" dengan nilai "member"
+        ]);
+
+        \App\Models\User::factory(300)->create([
+            'phone_number' => '34567899', // Ganti dengan nomor telepon yang sesuai
+            'role' => 'non-member', // Menambahkan kolom "role" dengan nilai "member"
+        ]);
+    }
+
+
+}
