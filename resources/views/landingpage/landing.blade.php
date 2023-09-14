11e0de306c39670dd5bd611073a5e28635ea1dc5
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-09-14 14:48:56 +0700

    update landing blade

--- resources/views/landingpage/landing.blade.php
+++ resources/views/landingpage/landing.blade.php
@@ -7,6 +7,7 @@
     <title>Website Komunitas Motor</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     {{-- font awesome CDN link --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
@@ -20,7 +21,7 @@
     <header class="header">
         <a href="#" class="logo">
             <img src="{{ asset('images/logo_wr.png') }}" alt="Logo">
-        </a>
+            </a>
         <form action="" class="search-form">
             <input type="search" name="" placeholder="cari di sini..." id="searchBox">
             <label for="searchBox"><i class="fas fa-search"></i></label>
@@ -72,13 +73,13 @@
         <video class="video-slide" src="{{ asset('videos/video_4.mp4') }}" autoplay muted loop></video>
         <div class="container">
 
-            <h1 class="h1 hero-title"  data-aos="zoom-in">
+            <h1 class="h1 hero-title" data-aos="zoom-in">
                 <marquee> Ride Together, Thrive Together <br></marquee>
             </h1>
 
-            <div class="btn-group"  data-aos="zoom-in">
+            <div class="btn-group" data-aos="zoom-in">
 
-                <button class="btn btn-secondary"  data-aos="zoom-in"><a href="https://goo.gl/maps/GhRswbtz19c7jGoV6"
+                <button class="btn btn-secondary" data-aos="zoom-in"><a href="https://goo.gl/maps/GhRswbtz19c7jGoV6"
                         style="color: #FFF;">Jelajahi Sekarang!</a></button>
             </div>
             <div class="slider-navigation">
@@ -105,7 +106,7 @@
                     <p>Bergabunglah bersama kami dalam perjalanan mendebarkan saat kami memulai acara Ride Adventures
                         pada tanggal 20 Agustus 2023! Siapkan diri Anda untuk menjelajahi rute-rute indah dan menjalin
                         persahabatan baru.</p>
-                        <a href="{{ route('event') }}" class="btn">Lihat Info Lebih Lanjut</a>
+                    <a href="{{ route('event') }}" class="btn">Lihat Info Lebih Lanjut</a>
                 </div>
             </div>
             <div class="box" data-aos="fade-up">
@@ -144,26 +145,31 @@
         </h1>
         <div class="swiper-container review-slider" data-aos="zoom-in">
             <div class="swiper-wrapper">
-        @foreach ($comment_post as $cp)
-        <div class="swiper-slide slide">
-            <img src="{{ asset('images/profile-icon-png-912.png') }}" alt="">
-            <h3>{{ $cp->username }}</h3>
-            <p>{{ $cp->content }}</p>
-            <div class="stars">
-                @for ($i = 1; $i <= 5; $i++)
-                    @if ($i <= $cp->rating)
-                        <i class="fas fa-star"></i>
-                    @else
-                        <i class="fas fa-star-half-alt"></i>
-                    @endif
-                @endfor
+                @foreach ($comment_post as $cp)
+                    <div class="swiper-slide slide">
+                        <img src="{{ asset('images/profile-icon-png-912.png') }}" alt="">
+                        <h3>{{ $cp->username }}</h3>
+                        <p>{{ $cp->content }}</p>
+                        <div class="stars">
+                            @php
+                            $rating = $cp->rating;
+                            $maxRating = 5;
+                        @endphp
+                        @for ($i = 1; $i <= $maxRating; $i++)
+                            @if ($i <= $rating)
+                                <i class='bx bxs-star'></i> <!-- Tampilkan bintang yang menyala -->
+                            @else
+                                <i class='bx bx-star'></i>
+                                <!-- Tampilkan bintang yang tidak menyala -->
+                            @endif
+                        @endfor
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
-    @endforeach
-</div>
-</div>
 
-            </div>
+        </div>
         </div>
     </section>
     {{-- ends --}}
@@ -176,8 +182,8 @@
             {{ session('success') }}
         </div>
     @endif --}}
-    <h1 class="heading"><span>Kritik</span> Saran</h1>
-    <div class="box-container" data-aos="fade-up"></div>
+        <h1 class="heading"><span>Kritik</span> Saran</h1>
+        <div class="box-container" data-aos="fade-up"></div>
         <form action="/submit-message" method="post" data-aos="zoom"
             onsubmit="return confirm('Terima kasih, pesan anda berhasil dikirim!')">
             @csrf
@@ -190,53 +196,57 @@
     </section>
     {{-- ends --}}
 
-   {{-- blogs --}}
-   <section class="blogs" id="others">
-    <h1 class="heading"><span>Berita</span> Terbaru</h1>
-    <div class="box-container" data-aos="fade-up">
+    {{-- blogs --}}
+    <section class="blogs" id="others">
+        <h1 class="heading"><span>Berita</span> Terbaru</h1>
+        <div class="box-container" data-aos="fade-up">
 
-        @foreach ($news as $nw)
-            <div class="box" data-aos="zoom">
-                <div class="image">
-                    <img src="{{ asset('storage/new_images/' . $nw->image) }}" alt="Events" loading="lazy">
-                </div>
-                <div class="content">
-                    <h3>{!! $nw->title !!}</h3>
-                    <a href="#" class="btn">Pelajari Selengkapnya</a>
-                    <div class="icons">
-                        <a href="#"><i class="fas fa-user"></i> by admin</a>
-                        <a href="#"><i class="fas fa-calendar"></i> {{ $nw->created_at }}</a>
+            @foreach ($news as $nw)
+                <div class="box" data-aos="zoom">
+                    <div class="image">
+                        <img src="{{ asset('storage/new_images/' . $nw->image) }}" alt="Events" loading="lazy">
+                    </div>
+                    <div class="content">
+                        <h3>{!! $nw->title !!}</h3>
+                        <a href="#" class="btn">Pelajari Selengkapnya</a>
+                        <div class="icons">
+                            <a href="#"><i class="fas fa-user"></i> by admin</a>
+                            <a href="#"><i class="fas fa-calendar"></i> {{ $nw->created_at }}</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
 
-    </div>
-</section>
+        </div>
+    </section>
 
-{{-- ends --}}
+    {{-- ends --}}
     {{-- footer --}}
     <section class="footer">
         <div class="box-container">
             <div class="box">
                 <h3>Our Communities</h3>
-                <a href="https://www.google.com/maps/search/?api=1&query=Bandung"><i class="fas fa-map-marker-alt"></i>Bandung</a>
-                <a href="https://www.google.com/maps/search/?api=1&query=Cimahi"><i class="fas fa-map-marker-alt"></i>Cimahi</a>
-                <a href="https://www.google.com/maps/search/?api=1&query=Bogor"><i class="fas fa-map-marker-alt"></i>Bogor</a>
-                <a href="https://www.google.com/maps/search/?api=1&query=Pangandaran"><i class="fas fa-map-marker-alt"></i>Pangandaran</a>
+                <a href="https://www.google.com/maps/search/?api=1&query=Bandung"><i
+                        class="fas fa-map-marker-alt"></i>Bandung</a>
+                <a href="https://www.google.com/maps/search/?api=1&query=Cimahi"><i
+                        class="fas fa-map-marker-alt"></i>Cimahi</a>
+                <a href="https://www.google.com/maps/search/?api=1&query=Bogor"><i
+                        class="fas fa-map-marker-alt"></i>Bogor</a>
+                <a href="https://www.google.com/maps/search/?api=1&query=Pangandaran"><i
+                        class="fas fa-map-marker-alt"></i>Pangandaran</a>
 
             </div>
             <div class="box">
                 <h3>Ikuti Kami</h3>
                 <a href="#" style="text-transform: none;"><i class="fas fa-phone"></i>+62 89687792980/a>
-                <a href="#" style="text-transform: none;"><i
-                        class="fa-brands fa-instagram"></i>wayangriders_id</a>
-                <a href="#" style="text-transform: none;"><i class="fa-brands fa-facebook-f"></i>wayang
-                    riders</a>
-                <a href="#" style="text-transform: none;"><i
-                        class="fa-brands fa-x-twitter"></i>wayangriders_id</a>
-                <a href="#" style="text-transform: none;"><i
-                        class="fa-regular fa-envelope"></i></i>wayangriders@gmail.com</a>
+                    <a href="#" style="text-transform: none;"><i
+                            class="fa-brands fa-instagram"></i>wayangriders_id</a>
+                    <a href="#" style="text-transform: none;"><i class="fa-brands fa-facebook-f"></i>wayang
+                        riders</a>
+                    <a href="#" style="text-transform: none;"><i
+                            class="fa-brands fa-x-twitter"></i>wayangriders_id</a>
+                    <a href="#" style="text-transform: none;"><i
+                            class="fa-regular fa-envelope"></i></i>wayangriders@gmail.com</a>
             </div>
         </div>
         <div class="credit">Dibuat Oleh : Komunitas Wayang Riders <span>| all rights reserved</span></div>
