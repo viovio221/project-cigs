6d06b5fc15af647b798bb0dc01476d3c08830dc9
azzam <azzamnoya123@gmail.com>
2023-09-16 17:25:08 +0700

    menambahkan controller kritik saran

---
+++ app/Http/Controllers/KritikController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+class KritikController extends Controller
+{
+    public function index()
+    {
+        return view("kritiksaran/index");
+    }
+}
