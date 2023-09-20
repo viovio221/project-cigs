7b78265dc078d176e3fa68c336b7abc9115e8554
unknown <jungw0npepsi@gmail.com>
2023-09-20 08:56:45 +0700

    add headline-text

--- database/migrations/2023_08_24_045604_create_messages_table.php
+++ database/migrations/2023_08_24_045604_create_messages_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('message');
+            $table->string('headline');
+            $table->string('text');
             $table->timestamps();
         });
     }
