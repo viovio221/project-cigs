2555e1091cd71bd0ecf1948b9e7635eb9b9399c6
keyyy17 <keyzakhalishmurad@gmail.com>
2023-09-23 09:01:40 +0700

    ini file yg readmore teh

---
+++ resources/views/readmore/readmore.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="readmoree.css">
+</head>
+<body>
+    <div class="container">
+        <div class="news-heading">NEWS</div>
+        <div class="yellow-bar"></div>
+        <div class="title"><b>Wayang Riders Officially Launched:</div></b>
+        <div class="description">
+          <b>A New Motorcycle Community Inspiring<br/>
+          The Spirit Of Brotherhood </b>
+        </div>
+        <div class="italic-description">Komunitas Wayang Riders, lakukan perjalanan sejauh 200 kilometer.</div>
+        <div class="white-box">
+          <img class="image" src="https://via.placeholder.com/500x332" />
+          <div class="website">WayangRiders.com-</div>
+          <div class="text">
+            Komunitas Motor Wayang Riders melakukan riding<br/>
+            Besar besaran dalam rangka merayakan ulang ta-<br/>
+            hun komunitas mereka yang ke 1 tahun, Riding ya-<br/>
+            ng bertemakan “Ga Rolling Ga Bandung”. Disini ke-<br/>
+            tua komunitas Wayang Riders (Muhammad Sidiq),<br/>
+            mengatakan bahwa, riding ini adalah riding terbaik<br/><br/>
+            “Menurut saya, riding ini menarik para pemotor lainnya untuk mengikuti komunitas<br/>kami ini” -ujar Muhammad Sidiq (Ketua umum Wayang Riders)
+          </div>
+        </div>
+      </div>
+
+</body>
+</html>
