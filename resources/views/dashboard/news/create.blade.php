34e9a8f333f644701ceb40ae20457b2c37a8d6d0
azzam <azzamnoya123@gmail.com>
2023-10-02 11:50:34 +0700

    all

--- resources/views/dashboard/news/create.blade.php
+++ resources/views/dashboard/news/create.blade.php
@@ -1,6 +1,7 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,28 +11,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- My CSS -->
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <!-- Custom CSS link -->
     <link rel="stylesheet" href="{{ asset('css/add_news.css') }}">
-    @if ($profiles->count() > 0)
-    <title>
-        @foreach ($profiles as $profile)
-            Edit Property | {{ $profile->community_name }}
-        @endforeach
-    </title>
-@endif
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    @foreach ($profiles as $item)
-    <title>Create News| {{ $item->community_name }}</title>
 
-    @endforeach
+</head>
 
-  </head>
-  <body>
-
- <!-- SIDEBAR -->
- <section id="sidebar">
+<body>
+   <!-- SIDEBAR -->
+<section id="sidebar">
     <a href="#" class="brand">
         <i class="fa-solid fa-motorcycle"></i>
         <span class="text">Wayang Riders</span>
@@ -171,8 +160,8 @@
     </section>
     <!-- SIDEBAR -->
 
-     <!-- CONTENT -->
-     <section id="content">
+    <!-- CONTENT -->
+    <section id="content">
         <!-- NAVBAR -->
         <nav>
             <i class='bx bx-menu'></i>
@@ -207,7 +196,7 @@
                         </li>
                         <li><i class='bx bx-chevron-right'></i></li>
                         <li>
-                            <a class="active" href="/dashboard/news">Back to News</a>
+                            <a class="active" href="/dashboard/news">Back to Events</a>
                         </li>
                     </ul>
                 </div>
@@ -215,39 +204,167 @@
 
         </main>
         <!-- MAIN -->
-
-<div class="container mt-5">
-    <form action="{{ route('news.store') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-          <label class="form-label">Title</label>
-          <input name="title" type="text" class="form-control @error('title') is-invalid @enderror">
-          @error('title')
-            <div class="invalid-feedback">{{ $message }}</div>
-        @enderror
-        </div>
-        <div class="form-group">
-            <label class="form-label">Description</label>
-            <input name="description" type="text" class="form-control @error('description') is-invalid @enderror">
-            @error('description')
-              <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-          </div>
-          <div class="mb-3">
-            <label for="image" class="form-label">Image</label>
-            <input type="file" name="image" class="form-control @error('image') is-invalid @enderror">
-            @error('image')
-                <div class="alert alert-danger mt-2">
-                    {{ $message }}
+    <div class="container mt-5">
+            <form action="{{ route('news.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                  <label class="form-label">Title</label>
+                  <input name="title" type="text" class="form-control @error('title') is-invalid @enderror">
+                  @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
                 </div>
-            @enderror
+                <br>
+                <div class="form-group">
+                    <label class="form-label">Description</label>
+                    <input name="description" type="text" class="form-control @error('description') is-invalid @enderror">
+                    @error('description')
+                      <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+                  </div>
+                  <br>
+                  <div class="mb-3">
+                    <label for="image" class="form-label">Image</label>
+                    <input type="file" name="image" class="form-control @error('image') is-invalid @enderror">
+                    @error('image')
+                        <div class="alert alert-danger mt-2">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <br><br>
+            <center><button type="submit" class="btn btn-primary">Submit</button></center>
+        </form>
         </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
+                            <!-- ... -->
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+
+                            <script>
+                                document.addEventListener('DOMContentLoaded', function() {
+                                    const registerButtons = document.querySelectorAll('.btn.btn-primary');
+
+                                            const eventId = registerButton.getAttribute('data-event-id');
+                                            const eventName = registerButton.getAttribute('data-event-name');
+                                            const eventDate = registerButton.getAttribute('data-event-date');
+
+                                            }).then((response) => {
+                                                if (response.data.message === 'You are already registered for this event.') {
+                                                    Swal.fire('You are already registered for this event.',
+                                                        'You cannot register again for the same event.', 'info');
+                                                } else if (response.data.message === 'Registration successful') {
+                                                    Swal.fire('Thank you!',
+                                                        'You have registered for this event.', 'success');
+                                                } else {
+                                                    Swal.fire('Error',
+                                                        'An error occurred while registering for the event.',
+                                                        'error');
+                                                }
+                                            }).catch((error) => {
+                                                Swal.fire('Error',
+                                                    'An error occurred while registering for the event.',
+                                                    'error');
+                                            });
+                                        });
+                                    });
+                                });
+                                </script>
+
+
+
+
+                            <!-- ... -->
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+        <!-- Ionicon link -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        <script>
+            AOS.init({
+                duration: 800,
+                delay: 400
+            });
+        </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            const searchBox = document.getElementById('searchBox');
+         const searchSubmit = document.getElementById('searchSubmit');
+         let alertShown = false;
+
+         searchSubmit.addEventListener('click', (e) => {
+             e.preventDefault();
+             const searchTerm = searchBox.value.toLowerCase();
+
+             if (searchTerm === '') {
+                 return;
+             }
+
+             removeHighlights();
+
+             const textElements = document.querySelectorAll('.searchable-element');
+
+             let found = false;
+             let firstMatchId = null;
+
+             textElements.forEach((element) => {
+                 const text = element.innerText.toLowerCase();
+                 const regex = new RegExp(searchTerm, 'gi');
+
+                 if (regex.test(text)) {
+                     found = true;
+                     const highlightedText = element.innerHTML.replace(
+                         regex,
+                         '<span class="highlight">$&</span>'
+                     );
+                     element.innerHTML = highlightedText;
+
+                     if (!firstMatchId) {
+                         firstMatchId = element.getAttribute('id');
+                     }
+                 }
+             });
+
+             if (found) {
+                 if (firstMatchId) {
+                     window.location.href = `#${firstMatchId}`;
+                 }
+             } else {
+                 if (!alertShown) {
+                     Swal.fire('Sorry!', 'No results for this search!', 'info');
+                     alertShown = true;
+                 } else {
+                     alertShown = false;
+                 }
+             }
+         });
+
+         function removeHighlights() {
+             const highlightedElements = document.querySelectorAll('.highlight');
+             highlightedElements.forEach((element) => {
+                 element.outerHTML = element.innerHTML;
+             });
+         }
+
+         </script>
+        <!-- CONTENT -->
+        <script src="{{ asset('js/dashboard.js') }}"></script>
+        @include('sweetalert::alert')
+        <!-- CONTENT -->
+
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        @include('sweetalert::alert')
+</body>
+
 </html>
