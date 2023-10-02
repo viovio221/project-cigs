45ae9bde32fb2d9fcc06cc0394d1d0a7f6230d97
keyyy17 <keyzakhalishmurad@gmail.com>
2023-10-02 10:45:03 +0700

    menambahkan create.blade

--- resources/views/dashboard/news/create.blade.php
+++ resources/views/dashboard/news/create.blade.php
@@ -1,10 +1,16 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News Description | Wayang Riders</title>
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- My CSS -->
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/add_news.css') }}">
     @if ($profiles->count() > 0)
     <title>Edit Property | {{ $profiles[0]->community_name }}</title>
     @foreach ($profiles as $profile)
@@ -14,6 +20,193 @@
 
   </head>
   <body>
+
+ <!-- SIDEBAR -->
+ <section id="sidebar">
+    <a href="#" class="brand">
+        <i class="fa-solid fa-motorcycle"></i>
+        <span class="text">Wayang Riders</span>
+    </a>
+    <ul class="side-menu top">
+        <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
+            <a href="{{ route('dashboard') }}">
+                <i class='bx bxs-dashboard'></i>
+                <span class="text">Dashboard</span>
+            </a>
+        </li>
+        @if (Auth::check() && Auth::user()->role === 'non-member')
+            <li class="{{ Request::is('dashboard/membersdata*') ? 'active' : '' }}">
+                <a href="/dashboard/membersdata">
+                    <i class='bx bxs-group'></i>
+                    <span class="text">Members Data</span>
+                </a>
+            </li>
+        @endif
+        @if (Auth::check() && Auth::user()->role === 'member')
+            <!-- Jika pengguna adalah member, tampilkan elemen sidebar tambahan -->
+            <li class="{{ Request::is('dashboard/event*') ? 'active' : '' }}">
+                <a href="/dashboard/event">
+                    <i class='bx bxs-shopping-bag-alt'></i>
+                    <span class="text">Events</span>
+                </a>
+            </li>
+            <li class="{{ Request::is('dashboard/news*') ? 'active' : '' }}">
+                <a href="/dashboard/news">
+                    <i class='bx bxs-message-dots'></i>
+                    <span class="text">News</span>
+                </a>
+            </li>
+            <li class="{{ Request::is('dashboard/membersdata*') ? 'active' : '' }}">
+                <a href="/dashboard/membersdata">
+                    <i class='bx bxs-group'></i>
+                    <span class="text">Members Data</span>
+                </a>
+            </li>
+        @endif
+        @if (Auth::check() && Auth::user()->role === 'admin')
+            <!-- Jika pengguna adalah admin, tampilkan elemen sidebar tambahan -->
+            <li class="{{ Request::is('dashboard/event*') ? 'active' : '' }}">
+                <a href="/dashboard/event">
+                    <i class='bx bxs-shopping-bag-alt'></i>
+                    <span class="text">Events</span>
+                </a>
+            </li>
+            <li class="{{ Request::is('dashboard/news*') ? 'active' : '' }}">
+                <a href="/dashboard/news">
+                    <i class='bx bxs-message-dots'></i>
+                    <span class="text">News</span>
+                </a>
+            </li>
+            <li class="{{ Request::is('dashboard/membersdata*') ? 'active' : '' }}">
+                <a href="/dashboard/membersdata">
+                    <i class='bx bxs-group'></i>
+                    <span class="text">Members Data</span>
+                </a>
+            </li>
+            <li class="{{ Request::is('dashboard/data_crud*') ? 'active' : '' }}">
+                <a href="#">
+                    <i class='bx bx-data'></i>
+                    <span class="text">CRUD Riders</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/event_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">Events</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/message_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">Message</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/commentposts_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">Comment Posts</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/news_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">News</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/setting_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">Setting</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/property_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">Property</span>
+                </a>
+            </li>
+            <li class="side1">
+                <a href="/dashboard/membersdata_crud" class="text2">
+                    <i class='bx bx-chevrons-right'></i> <span class="text">Confirm User</span>
+                </a>
+            </li>
+    </ul>
+    @endif
+    <ul class="side-menu">
+        <li>
+            <a href="#" class="logout">
+                <i class='bx bxs-log-out-circle'></i>
+                <span class="text">Logout</span>
+            </a>
+        </li>
+    </ul>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const logoutButton = document.querySelector('.logout');
+
+                // Tambahkan event click ke elemen logout
+                logoutButton.addEventListener('click', function(e) {
+                    e.preventDefault(); // Mencegah tindakan logout asli
+
+                    // Tampilkan pesan konfirmasi SweetAlert2
+                    Swal.fire({
+                        title: 'Are you sure to logout?',
+                        text: "You won't be able to revert this!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#ffa500',
+                        cancelButtonColor: '#DB504A',
+                        confirmButtonText: 'Yes, logout'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = '{{ route('logout') }}';
+                        }
+                    });
+                });
+            });
+        </script>
+    </section>
+    <!-- SIDEBAR -->
+
+     <!-- CONTENT -->
+     <section id="content">
+        <!-- NAVBAR -->
+        <nav>
+            <i class='bx bx-menu'></i>
+            <form id="searchForm">
+                <div class="form-input">
+                    <input type="search" id="searchBox" placeholder="Cari...">
+                    <button type="submit" id="searchSubmit" class="search-btn"><i class='bx bx-search'></i></button>
+                </div>
+            </form>
+            <a href="/dashboard/message" class="notification">
+                <i class='bx bxs-edit-alt'></i>
+            </a>
+            <input type="checkbox" id="switch-mode" hidden>
+            <label for="switch-mode" class="switch-mode"></label>
+            <a href="/dashboard/review" class="notification">
+                <i class='bx bxs-bell'></i>
+            </a>
+
+            <a href="{{ route('editprofile.show') }}" class="notification" title="edit profile here">
+                <i class='bx bxs-user-circle'></i>       </a>
+        </nav>
+        <!-- NAVBAR -->
+
+        <!-- MAIN -->
+        <main>
+            <div class="head-title">
+                <div class="left">
+                    <h1>News Description</h1>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="/dashboard">Dashboard</a>
+                        </li>
+                        <li><i class='bx bx-chevron-right'></i></li>
+                        <li>
+                            <a class="active" href="/dashboard/news">Back to News</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </main>
+        <!-- MAIN -->
+
 <div class="container mt-5">
     <form action="{{ route('news.store') }}" method="post" enctype="multipart/form-data">
         @csrf
