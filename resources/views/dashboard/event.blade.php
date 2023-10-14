bff21ab5b1c369a79dce4e9452372f1ae15398c7
unknown <jungw0npepsi@gmail.com>
2023-10-14 13:21:10 +0700

    feat: event blade

--- resources/views/dashboard/event.blade.php
+++ resources/views/dashboard/event.blade.php
@@ -238,12 +238,25 @@
                                         </ul>
                                     </div>
                                     <div class="card-price">
-
+                                        @foreach ($events as $evco)
+                                            <td>
+                                                @if ($evco->is_registered)
+                                                    <?php
+                                                    $kode = $evco->id . '/' . 'wayangriders/' . $evco->name . '';
+                                                    require_once 'qrcode/qrlib.php';
+                                                    $filename = 'wayangriders' . $evco->id . '.png';
+                                                    $path = storage_path('app/public/presence_images/' . $filename);
+                                                    QRcode::png("$kode", $path, 2, 2);
+                                                    ?>
+                                                    <img src="{{ asset('storage/presence_images/' . $filename) }}" alt="">
+                                                @endif
+                                            </td>
+                                        @endforeach
                                         <button class="btn btn-secondary searchable-element">
-                                            <a href="{{ route('event.show', $item->id) }}" style="color: white;">See
-                                                Description</a>
+                                            <a href="{{ route('event.show', $item->id) }}" style="color: white;">See Description</a>
                                         </button>
                                     </div>
+
                                 </div>
                             </li>
                         @endforeach
