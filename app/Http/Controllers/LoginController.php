1ffc0761615a11d74e2a5cc57be7d78846aac4ec
Devani Amelia Pratiwi <devaniamelia2018@gmail.com>
2023-10-21 11:47:32 +0700

    feat: login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
@@ -36,16 +37,24 @@ class LoginController extends Controller
             $otpExpiryTime = $user->otp_expiry_time;
 
             if ($otpExpiryTime && now() <= $otpExpiryTime && $enteredOTP == $user->otp_code) {
+                if ($user->role === 'non-member') {
+                    $recipientNumber = $user->phone_number;
+                    $message = "{$user->name} Selamat datang di Komunitas Motor Wayang Riders! Terima kasih sudah menjadi bagian dari komunitas kami. Jika Anda belum menjadi anggota, inilah saat yang tepat untuk bergabung! Mari bersama-sama membangun kenangan dan pengalaman baru di komunitas motor Wayang Riders. Mohon lengkapi identitas Anda melalui link berikut ini: https://wayang.kakara.my.id/editprofile";
+
+                    $response = Http::post('https://wag.cigs.web.id/send-message', [
+                        'api_key' => 'ZMNgdCuH1Vi0OCQ6Recg8ZB9UPy68B',
+                        'sender' => '6282128078893',
+                        'number' => $recipientNumber,
+                        'message' => $message,
+                    ]);
+                }
+
                 return redirect()->route('dashboard');
             } else {
                 return back()->withErrors(['otp' => 'Invalid OTP code.'])->withInput();
             }
-        } else {
-            return back()->withErrors(['email' => 'The username and password entered do not match'])->withInput();
         }
     }
-
-
 
     public function logout()
     {
